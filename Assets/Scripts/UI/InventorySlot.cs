69e5ce6b0d70edc0b3ee6530f3f0f5567d870f40
Orbry <orbryak@gmail.com>
2021-09-18 13:25:29 +0300

    added inventory slots, refactored StatBar script
    Moved the setup of components and child nodes into prefabs via serialized private fields

---
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] private Sprite m_DefaultSprite;
+    [SerializeField] private string m_DefaultName;
+
+    [SerializeField] private TMP_Text m_TextNode;
+    [SerializeField] private Image m_ImageNode;
+
+    private void Start()
+    {
+        // making image transparent first in case there is no icon set
+        m_ImageNode.color = Color.clear;
+        
+        if (m_DefaultSprite != null)
+        {
+            m_ImageNode.sprite = m_DefaultSprite;
+            m_ImageNode.color = Color.white;
+        }
+
+        m_TextNode.text = m_DefaultName;
+    }
+}
--- Assets/Scripts/UI/StatBar.cs
+++ Assets/Scripts/UI/StatBar.cs
@@ -2,34 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 /* A class that simulates UI bars with non-interactable horizontal scrollbars
  */
 public class StatBar : MonoBehaviour
 {
-    private Scrollbar m_Scrollbar;
-    private GameObject m_Handle;
+    [SerializeField] private Scrollbar m_Scrollbar;
+    [SerializeField] private Image m_HandleImage;
+    [SerializeField] private Color m_BarColor;
 
-    public Color color;
-
-    private void Awake()
-    {
-        m_Scrollbar = GetComponent<Scrollbar>();
-        m_Handle = gameObject.transform.GetChild(0).GetChild(0).gameObject;
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
         m_Scrollbar.interactable = false;
         m_Scrollbar.size = Random.Range(0f, 1f);
 
-        m_Handle.GetComponent<Image>().color = color;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (m_BarColor != null)
+        {
+            m_HandleImage.color = m_BarColor;
+        }
     }
 }
