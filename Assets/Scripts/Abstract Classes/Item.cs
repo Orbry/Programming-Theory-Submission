1f27d9c9808dd34e66b47ee134c7038f8ecc9033
Orbry <orbryak@gmail.com>
2021-09-20 11:01:03 +0300

    added outline on hover to items

--- Assets/Scripts/Abstract Classes/Item.cs
+++ Assets/Scripts/Abstract Classes/Item.cs
@@ -7,12 +7,14 @@ using UnityEngine.EventSystems;
 using UnityEditor;
 #endif
 
-public abstract class Item : MonoBehaviour, IPointerClickHandler
+public abstract class Item : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Item settings")]
     [SerializeField] protected string m_ItemName;
     [SerializeField] protected Sprite m_ItemIcon;
     [SerializeField] protected SpriteRenderer m_SpriteRenderer;
+    [SerializeField] private Material m_OnHoverMaterial;
+    [SerializeField] private Material m_DefaultMaterial;
 
     public string ItemName => m_ItemName;
 
@@ -25,6 +27,16 @@ public abstract class Item : MonoBehaviour, IPointerClickHandler
     }
 
     public abstract void OnPointerClick(PointerEventData eventData);
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_SpriteRenderer.material = m_OnHoverMaterial;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_SpriteRenderer.material = m_DefaultMaterial;
+    }
 
     # if UNITY_EDITOR
     // Updating sprite in Scene View while not in Play Mode
