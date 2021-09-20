1c8156c2627c768a9c2dd54f4b36b7a14d9c75ba
Orbry <orbryak@gmail.com>
2021-09-20 16:47:08 +0300

    added vfx for mouse click

---
+++ Assets/Scripts/ClickVFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickVFX : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clickPosition.z = 0;
+            
+            ParticleSystem fx = GameObject.Instantiate(particle);
+            fx.gameObject.transform.position = clickPosition;
+        }
+    }
+}
